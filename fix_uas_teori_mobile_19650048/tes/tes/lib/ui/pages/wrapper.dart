1132cc48163bd447279c790fd489f5c9e919ddaf
afrizalproject <afrizalrizky777@gmail.com>
2021-12-15 08:49:44 +0700

    done sign up page part 1

--- fix_uas_teori_mobile_19650048/tes/tes/lib/ui/pages/wrapper.dart
+++ fix_uas_teori_mobile_19650048/tes/tes/lib/ui/pages/wrapper.dart
@@ -23,6 +23,8 @@ class Wrapper extends StatelessWidget {
             ? SplashPage()
             : (pageState is OnLoginPage)
                 ? SignInPage()
-                : MainPage());
+                : (pageState is OnRegistrationPage)
+                    ? SignUpPage(pageState.registrationData)
+                    : MainPage());
   }
 }
