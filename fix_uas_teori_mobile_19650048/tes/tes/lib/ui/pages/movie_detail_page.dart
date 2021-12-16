798b6c4ff08513ed84d44c235857d87b8e451acb
afrizalproject <afrizalrizky777@gmail.com>
2021-12-16 09:39:22 +0700

    movie detail page part 2 done

---
+++ fix_uas_teori_mobile_19650048/tes/tes/lib/ui/pages/movie_detail_page.dart
@@ -0,0 +1,49 @@
+part of 'pages.dart';
+
+class MovieDetailPage extends StatelessWidget {
+  final Movie movie;
+  MovieDetailPage(this.movie);
+  @override
+  Widget build(BuildContext context) {
+    MovieDetail movieDetail;
+    List<Credit> credits;
+    return WillPopScope(
+      onWillPop: () async {
+        context.bloc<PageBloc>().add(GoToMainPage());
+        return;
+      },
+      child: Scaffold(
+        body: FutureBuilder(
+          future: MovieServices.getDetails(movie),
+          builder: (_, snapshot) {
+            if (snapshot.hasData) {
+              movieDetail = snapshot.data;
+              return FutureBuilder(
+                future: MovieServices.getCredits(movie.id),
+                builder: (_, snapshot) {
+                  if (snapshot.hasData) {
+                    credits = snapshot.data;
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(movieDetail.title),
+                        Text(movieDetail.genresAndLanguage),
+                        Column(
+                          children: credits.map((e) => Text(e.name)).toList(),
+                        )
+                      ],
+                    );
+                  } else {
+                    return SizedBox();
+                  }
+                },
+              );
+            } else {
+              return SizedBox();
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
--- fix_uas_teori_mobile_19650048/tes/tes/lib/ui/pages/movie_page.dart
+++ fix_uas_teori_mobile_19650048/tes/tes/lib/ui/pages/movie_page.dart
@@ -4,101 +4,92 @@ class MoviePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: [
-        //note: HEADER
+      children: <Widget>[
+        // note: HEADER
         Container(
-            decoration: BoxDecoration(
-                color: accentColor1,
-                borderRadius: BorderRadius.only(
+          decoration: BoxDecoration(
+              color: accentColor1,
+              borderRadius: BorderRadius.only(
                   bottomLeft: Radius.circular(20),
-                  bottomRight: Radius.circular(20),
-                )),
-            padding: EdgeInsets.fromLTRB(defaultMargin, 20, defaultMargin, 30),
-            child: BlocBuilder<UserBloc, UserState>(
-              builder: (_, userState) {
-                if (userState is UserLoaded) {
-                  var numberFormat = NumberFormat.currency(
-                    locale: "id_ID",
-                    decimalDigits: 0,
-                  );
-                  return Row(
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          border:
-                              Border.all(color: Color(0xFF5F558B), width: 1),
+                  bottomRight: Radius.circular(20))),
+          padding: EdgeInsets.fromLTRB(defaultMargin, 20, defaultMargin, 30),
+          child: BlocBuilder<UserBloc, UserState>(builder: (_, userState) {
+            if (userState is UserLoaded) {
+              return Row(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(color: Color(0xFF5F558B), width: 1)),
+                    child: Stack(
+                      children: <Widget>[
+                        SpinKitFadingCircle(
+                          color: accentColor2,
+                          size: 50,
                         ),
-                        child: Stack(
-                          children: [
-                            SpinKitFadingCircle(
-                              color: accentColor2,
-                              size: 50,
-                            ),
-                            Container(
-                              width: 50,
-                              height: 50,
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  image: DecorationImage(
-                                      image: (userState.user.profilePictures ==
-                                              ""
-                                          ? AssetImage("assets/user_pic.png")
-                                          : NetworkImage(
-                                              userState.user.profilePictures)),
-                                      fit: BoxFit.cover)),
-                            )
-                          ],
-                        ),
-                      ),
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                  image: (userState.user.profilePictures == ""
+                                      ? AssetImage("assets/user_pic.png")
+                                      : NetworkImage(
+                                          userState.user.profilePictures)),
+                                  fit: BoxFit.cover)),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
                       SizedBox(
-                        width: 16,
+                        width: MediaQuery.of(context).size.width -
+                            2 * defaultMargin -
+                            78,
+                        child: Text(
+                          userState.user.name,
+                          style: whiteTextFont.copyWith(fontSize: 18),
+                          maxLines: 1,
+                          overflow: TextOverflow.clip,
+                        ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width -
-                                2 * defaultMargin -
-                                78,
-                            child: Text(
-                              userState.user.name,
-                              style: whiteTextFont.copyWith(fontSize: 18),
-                              maxLines: 1,
-                              overflow: TextOverflow.clip,
-                            ),
-                          ),
-                          Text(
-                            NumberFormat.currency(
-                                    locale: "id_ID",
-                                    decimalDigits: 0,
-                                    symbol: "IDR ")
-                                .format(userState.user.balance),
-                            style: yellowNumberFont.copyWith(
-                                fontSize: 14, fontWeight: FontWeight.w400),
-                          )
-                        ],
+                      Text(
+                        NumberFormat.currency(
+                                locale: "id_ID",
+                                decimalDigits: 0,
+                                symbol: "IDR ")
+                            .format(userState.user.balance),
+                        style: yellowNumberFont.copyWith(
+                            fontSize: 14, fontWeight: FontWeight.w400),
                       )
                     ],
-                  );
-                } else {
-                  return SpinKitFadingCircle(
-                    color: accentColor2,
-                    size: 50,
-                  );
-                }
-              },
-            )),
+                  )
+                ],
+              );
+            } else {
+              return SpinKitFadingCircle(
+                color: accentColor2,
+                size: 50,
+              );
+            }
+          }),
+        ),
 
-        //note: NOW PLAYING
+        // note: NOW PLAYING
         Container(
           margin: EdgeInsets.fromLTRB(defaultMargin, 30, defaultMargin, 12),
-          child: Text("Now Playing",
-              style: blackTextFont.copyWith(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              )),
+          child: Text(
+            "Now Playing",
+            style: blackTextFont.copyWith(
+                fontSize: 18, fontWeight: FontWeight.bold),
+          ),
         ),
         SizedBox(
           height: 140,
@@ -106,17 +97,25 @@ class MoviePage extends StatelessWidget {
             builder: (_, movieState) {
               if (movieState is MovieLoaded) {
                 List<Movie> movies = movieState.movies.sublist(0, 10);
+
                 return ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: movies.length,
-                  itemBuilder: (_, index) => Container(
-                    margin: EdgeInsets.only(
-                        left: (index == 0) ? defaultMargin : 0,
-                        right:
-                            (index == movies.length - 1) ? defaultMargin : 16),
-                    child: MovieCard(movies[index]),
-                  ),
-                );
+                    scrollDirection: Axis.horizontal,
+                    itemCount: movies.length,
+                    itemBuilder: (_, index) => Container(
+                          margin: EdgeInsets.only(
+                              left: (index == 0) ? defaultMargin : 0,
+                              right: (index == movies.length - 1)
+                                  ? defaultMargin
+                                  : 16),
+                          child: MovieCard(
+                            movies[index],
+                            onTap: () {
+                              context
+                                  .bloc<PageBloc>()
+                                  .add(GoToMovieDetailPage(movies[index]));
+                            },
+                          ),
+                        ));
               } else {
                 return SpinKitFadingCircle(
                   color: mainColor,
@@ -127,7 +126,7 @@ class MoviePage extends StatelessWidget {
           ),
         ),
 
-        //note: Browse Movie
+        // note: BROWSE MOVIE
         Container(
           margin: EdgeInsets.fromLTRB(defaultMargin, 30, defaultMargin, 12),
           child: Text(
@@ -158,7 +157,7 @@ class MoviePage extends StatelessWidget {
           },
         ),
 
-        //note: Coming Soon
+        // note: COMING SOON
         Container(
           margin: EdgeInsets.fromLTRB(defaultMargin, 30, defaultMargin, 12),
           child: Text(
@@ -195,7 +194,7 @@ class MoviePage extends StatelessWidget {
           ),
         ),
 
-        //note: LUCKY DAY
+        // note: GET LUCKY DAY
         Container(
           margin: EdgeInsets.fromLTRB(defaultMargin, 30, defaultMargin, 12),
           child: Text(
